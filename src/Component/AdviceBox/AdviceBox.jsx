8a82af881752777bad7444b586b57936b54e4ee5
uzma-nazim <uuzzi0216@gmail.com>
2024-02-19 23:40:13 +0500

    build error solved

--- src/Component/AdviceBox/AdviceBox.jsx
+++ src/Component/AdviceBox/AdviceBox.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from "react";
 import surveyVector from "@/images/advice.jpg";
 import bg from "@/images/bg.jpg";
@@ -141,7 +142,9 @@ const AdviceBox = () => {
       setadvice(htmlContent);
     }
   }, [router]);
-
+  if (!router) {
+    return <div>Loading...</div>; // You can customize the loading indicator
+  }
   return (
     <div
       style={{
--- src/app/(routes)/advice/page.js
+++ src/app/(routes)/advice/page.js
@@ -2,12 +2,14 @@
 import AdviceBox from '@/Component/AdviceBox/AdviceBox'
 import Header from '@/Component/Header/Header'
 import React from 'react'
-
+import { Suspense } from 'react'
 const index = () => {
   return (
     <>
     <Header/>
+    <Suspense>
     <AdviceBox/>
+    </Suspense>
     </>
   )
 }
